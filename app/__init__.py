25ec86bfb7befceb4ba3dcd6b3491ee07f19a1ef
Ahmed Aly <ahmedelshobki@gmail.com>
2023-11-20 19:26:15 +0200

    modified microblog strings to be translated using flask_babel

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 from config import Config
 
@@ -16,6 +16,11 @@ from flask_bootstrap import Bootstrap
 
 from flask_moment import Moment
 
+from flask_babel import Babel, lazy_gettext as _l
+
+def get_locale():
+    
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
 
@@ -26,12 +31,15 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please login to access this page!')
 
 mail = Mail(app)
 
 bootstrap = Bootstrap(app)
 
 moment = Moment(app)
+
+babel = Babel(app, locale_selector=get_locale)
 
 if not app.debug:
 
@@ -64,5 +72,7 @@ if not app.debug:
     app.logger.addHandler(file_handler)
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+
 
 from app import routes, models, errors
